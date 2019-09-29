623dd4221077a822e2204f5815549dfb4f44bd64
Yousef <yousef.r.haggy@gmail.com>
2019-09-28 23:35:12 -0400

    google signin

--- Avalanche/App.js
+++ Avalanche/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons'
-import {  createAppContainer} from 'react-navigation'
-import {createBottomTabNavigator} from 'react-navigation-tabs'
-import {createStackNavigator} from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createStackNavigator } from 'react-navigation-stack'
 import DebtScreen from './DebtScreen'
 import ProfileScreen from './ProfileScreen'
+import SigninScreen from './SigninScreen'
+
 
 const DebtStack = createStackNavigator({
-    Debts: { screen: DebtScreen }   
+    Signin:  {screen :SigninScreen,navigationOptions: () => ({
+      title: `Sign in`,
+     
+    })} ,
+    Debts: {screen:DebtScreen} 
 }, {
     defaultNavigationOptions: {
         headerStyle: {
             backgroundColor: '#7b1fa2'
         },
         headerTintColor: '#FFFFFF',
+        headerLeft:null,
+        gesturesEnabled:false,
         title: 'Debts',
     }
 });
+DebtStack.navigationOptions = ({ navigation }) => {
+  let tabBarVisible = true;
+  if (navigation.state.routes.routeName == "Signin") {
+    tabBarVisible = false;
+  }
+
+  return {
+    tabBarVisible,
+  };
+};
 const ProfileStack = createStackNavigator({
-    Profile: { screen: ProfileScreen }   
+    Profile: { screen: ProfileScreen }
 }, {
     defaultNavigationOptions: {
         headerStyle: {
@@ -35,7 +53,9 @@ const App = createBottomTabNavigator({
     Profile: { screen: ProfileStack }
 }, {
     defaultNavigationOptions: ({ navigation }) => ({
+        
         tabBarIcon: ({ focused, horizontal, tintColor }) => {
+
             const { routeName } = navigation.state;
             let IconComponent = Ionicons;
             let iconName;
@@ -44,6 +64,8 @@ const App = createBottomTabNavigator({
             } else if (routeName == "Profile") {
                 iconName = 'md-person'
             }
+            if (routeName == 'Signin')
+                return null
             return <IconComponent name={iconName} size={25} color={tintColor} />;
         },
 
@@ -56,5 +78,4 @@ const App = createBottomTabNavigator({
         },
     },
 });
-
 export default createAppContainer(App);
--- Avalanche/SigninScreen.js
+++ Avalanche/SigninScreen.js
@@ -1,18 +1,82 @@
 import React, { Component } from 'react'
-import { StyleSheet, View} from 'react-native'
+import { StyleSheet, View, Text, Button} from 'react-native'
+import * as Google from "expo-google-app-auth"
 export default class SigninScreen extends Component{
+	constructor(props){
+		super(props)
+		this.state={signedIn:false, name:"",id:0}
+	}
+
+	signIn = async () => {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId: "70054762554-qigq39s5dqp4joqc1hlopepaum77i5mv.apps.googleusercontent.com",
+        //iosClientId: YOUR_CLIENT_ID_HERE,  <-- if you use iOS
+        scopes: ["profile", "email"]
+      })
+
+       if (result.type === "success") {
+        this.setState({
+          signedIn: true,
+          name: result.user.name,
+          id:result.user.id
+        })
+        this.props.navigation.replace('Debts',{name:this.state.name,id:this.state.id})
+      } else {
+        console.log("cancelled")
+      }
+    } catch (e) {
+      console.log("error", e)
+    }
+}
+
+
 	render(){
-	return(
-		            <View style={styles.container}>
-		            <Text></Text>
-		            </View>
-		);
+	return (
+      <View style={styles.container}>
+        {this.state.signedIn ? (
+          <LoggedInPage name={this.state.name} id={this.state.id}  />
+        ) : (
+          <LoginPage signIn={this.signIn} />
+        )}
+      </View>
+    )
 	}
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'flex-start',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
-});
+  header: {
+    fontSize: 25
+  },
+  image: {
+    marginTop: 15,
+    width: 150,
+    height: 150,
+    borderColor: "rgba(0,0,0,0.2)",
+    borderWidth: 3,
+    borderRadius: 150
+  }
+})
+const LoginPage = props => {
+  return (
+    <View>
+      <Text style={styles.header}>Sign In With Google</Text>
+      <Button title="Sign in with Google" 
+       onPress={() => props.signIn()} />
+    </View>
+  )
+}
+const LoggedInPage = props => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Welcome:{props.name}</Text>
+       <Text style={styles.header}>Welcome:{props.id}</Text>
+
+    </View>
+  )
+}
